6dd836581b93a92a3ba9d4c5078e25a82226172a
Jen Liu <fl1122@nyu.edu>
2016-12-17 14:49:13 -0500

    Initial Commit

---
+++ Lab3branch/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab3branch
+//
+//  Created by Jen Liu on 12/17/16.
+//  Copyright © 2016 Jen Liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
